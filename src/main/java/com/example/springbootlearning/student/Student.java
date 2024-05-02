57938eb76e9c1697c4e418a0066267d656746bb0
rubinnum <romarypiak@gmail.com>
2024-05-02 21:59:14 +0200

    Data Access Layer implemented with one error

--- src/main/java/com/example/springbootlearning/student/Student.java
+++ src/main/java/com/example/springbootlearning/student/Student.java
@@ -1,11 +1,24 @@
 package com.example.springbootlearning.student;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Data @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private long id;
     private String name;
     private String email;
--- src/main/java/com/example/springbootlearning/student/StudentService.java
+++ src/main/java/com/example/springbootlearning/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.springbootlearning.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,10 +10,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1, "Roman", "romarypiak@gmail.com", LocalDate.of(2004, Month.NOVEMBER, 16), 19)
-        );
+        return studentRepository.findAll();
     }
 
 }
